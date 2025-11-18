f215d4b0240081b7b4adfe6badbc4d49945e8a62
Zachery Wampler <145584345+Ztw4322@users.noreply.github.com>
2025-11-17 22:07:44 -0500

    Update login.jsx

--- client/login.jsx
+++ client/login.jsx
@@ -71,7 +71,7 @@ const SignupWindow = (props) => {
       <input id='user' type="text" name='username' placeholder='username' />
       <label htmlFor="pass">Password: </label>
       <input id='pass' type="password" name='pass' placeholder='password' />
-      <label htmlFor="pass">Password: </label>
+      <label htmlFor="pass2">Password: </label>
       <input id='pass2' type="password" name='pass2' placeholder='retype password' />
       <input type="submit" value="Sign Up" className='formSubmit' />
     </form>
